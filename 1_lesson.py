a33e9b648a54c8b3ae7fdb37b72c3dcad968121d
zahiriddindtb <135936492+zahiriddindtb@users.noreply.github.com>
2023-06-08 08:56:27 +0500

    bu mening pythondagi birinchi darsligim

---
+++ 1_lesson.py
@@ -0,0 +1,42 @@
+imput(''isming ayt'')
+imput(''familyangni ayt'')
+imput(''yoshingni ayt'')
+a = 'imput'(''kvadrat tomoni: '')
+P = 4*a
+print(p)
+a = int(imput(''kvadrat tomoni: ''))
+p = 4*a
+print(p)
+b = int(imput(''to'g'ri to'rtburchak tomonlari: '')
+S = a*b
+p = 2*(a+b)
+print(S)
+print(P)
+c = int(imput(''to'g'ri turtburchak: '')
+Z = p*d
+p = 4.14
+print(Z)
+print(P)
+d = int(imput(aylanani uzunligi: '')
+L = p*d
+p = 3.14
+print(L)
+print(p)
+s = int(imput(kubning hajmi: '')
+V = a**3
+S = 6-a**2
+print(V)
+print(S)
+k = int(imput(misloning javobi: '')
+h = 4*5
+print(h)
+
+
+
+
+
+
+
+
+
+
