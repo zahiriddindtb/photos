35bfe4e028134901c8911d717fb19ad47409aa50
zahiriddindtb <135936492+zahiriddindtb@users.noreply.github.com>
2023-06-08 08:54:38 +0500

    bu mening pythondagi darsim

---
+++ 8_lesson.py
@@ -0,0 +1,280 @@
+#a>b
+#a<b
+#a==b
+#a!=b
+#a>b and a>c
+#print(a>b and a>c )
+#yosh = 19
+#passport = False
+#print(yosh>18 and passport == True)
+#print(yosh>18 or passport == True)
+#order = 'ha'
+#if order == 'ha':
+#    print('switch of')
+#elif order == 'yoq':
+#    print('switch on')
+#age = 18
+#if age==18:
+#    if age>30:
+#        print('hi')
+#if age==18 and age<30:
+#   if age ==18 and age <30:
+#       print('hi')
+#1-misol
+#a = int(input('butun son')) 
+#b=a > 0
+#print(a)
+#2-misol
+#a = int(input('butun son'))
+#b=a/2
+#print(b)
+#3-misol
+#a = int(input('butun son'))
+#b=a/2
+#print(b)
+#4-misol
+#a>2
+#b<=3
+#print(a>2 and b<=3)
+#5-misol
+#a>=0
+#b<-2
+#print(a>=0 or b<-2)
+#6-misol
+#a<=b
+#b<=a
+#print('a<=b and b<=c')
+#7-misol
+#a>b
+#b>a
+#print('a>b and b>c')
+#8-misol
+#a/=2
+#b/=2
+#print('a/=2 and b/=2')
+#9-misol
+#a/=2
+#b/=2
+#print('a/=2 or b/=2')
+#10-misol
+#a/=2
+#b/=2
+#print('a/=2 and b/=2')
+#23-misol
+#a = 111
+#b = 111
+#a==b
+#b==a
+#print(a==b and b==a)
+#a = 3
+#if a>=0:
+#    print('butun son')
+#else:
+#    print('butun son emas')
+#2-misol
+#a = 5
+#if a>=0-2:
+#    print('musbat son')
+#else:
+#    print('manfity son')
+#a = 4
+#if a>=0+1:
+#     print('musbat son')
+#if a<=0-2:
+#    print('manfuiy son')
+#a = 2
+#b = 3
+#c = 4
+#if a>=0 and b>=0:
+#    print('musbat son')
+#a = 3
+#b = 5
+#c = 7
+#if a>=0 .b>=0 and c<=0:
+#    print('musbat and manfiy')
+#a = 4
+#b = 7
+#if a>=b or b>=a:
+#    print('katta son')
+#a = 4
+#b = 7
+#if a<=b or b<=a:
+#    print('kichik son')
+#a = 6
+#b = 4
+#if a>b:
+#    a = b
+#    b = c 
+#    print('a=kichik and b=katta son')
+#a = int(input(''))
+#b = int(input(''))
+#c = 5 
+#if a!=b:
+#    a = b
+#    b = c
+#    print(a=b)
+#a = int(input(''))
+#b = int(input(''))
+#if a!=b:
+#    a = b 
+#    b = a
+#    print(a and b)
+#a = int(input(''))
+#b = int(input(''))
+#c = int(input(''))
+#if a>b>c:
+#   a > b 
+#    b > c
+#    print('eng kichik son')
+#a = 3 
+#b = 4
+#c = 5
+#if a>b>c:
+#     a>b 
+#     b>c
+#     print('a>b and b>c')
+#a = int(input(''))
+#b = int(input(''))
+#c = int(input(''))
+#if c<b<a:
+#    c<b
+#    b<a
+#    print(c<b and b<a)     
+#a = 7
+#b = 5
+#c = 3
+#if a>b>c:
+#    a+b>c
+#    print(a+b>c)
+#a = 1
+#b = 2
+#c = 3
+
+
+parol = 12345678
+parol = int(input('parolni kiriting: '))
+if parol==12345678:
+    print('Xurmatli Asliddinov Zahiriddin parolingiz togri va siz bu saytga kirishingiz mumkin!')
+else:
+    print('parol xato va siz bu saytga kira olmaysiz') 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
